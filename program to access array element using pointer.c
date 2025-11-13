e2ad6dfd4ded7debec8432f000f40ca2e8eb9268
chithrachithra87429-sys <chithrachithra87429@gmail.com>
2025-11-13 21:26:05 +0530

    Add files via upload

---
+++ program to access array element using pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    int *p = arr;
+    printf("array belements using pointer:");
+    for(int i=0;i<5;i++)
+        printf("%d",*(p+i));
+     return 0;
+
+   }
